f50b7eb59108c744a32be7a7ed101c3a73f8657b
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2013-06-05 23:54:36 +0900

    Prototyped OAuth2 implementation to fetch authenticated user's gists

--- popup.js
+++ popup.js
@@ -1,28 +1,37 @@
 $(function(){
 
-  function get(url, success) {
+  var authorized = $('button#github.authorized');
+
+  if (authorized.length) {
+    console.log('authorized!');
+
+    var provider = window['github'];
+    var data = {
+      access_token: provider.getAccessToken()
+    }
+
+    get('https://api.github.com/gists', data, function(gists){
+      for (var i = 0; i < gists.length; i++) {
+        var gist = gists[i];
+        var name = '';
+        for (var key in gist.files) {
+          name = key;
+          break;
+        }
+        var html = '<div><a href="' + gist.html_url + '">' + name + '</div>';
+        $('body').append(html);
+      }
+    });
+  }
+
+  function get(url, data, success) {
     $.ajax({
       type: 'GET',
       timeout: '5000',
       url: url,
+      data: data,
       success: success
     });
   }
-
-  get('https://api.github.com/gists', function(gists){
-
-    for (var i = 0; i < gists.length; i++) {
-      var gist = gists[i];
-
-      var name = '';
-      for (var key in gist.files) {
-        name = key;
-        break;
-      }
-
-      var html = '<div><a href="' + gist.html_url + '">' + name + '</div>';
-      $('body').append(html);
-    }
-  });
 
 });
